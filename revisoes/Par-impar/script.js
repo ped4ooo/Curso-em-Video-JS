e6f4e84df199baa69202fc47cb30c018fc2e6637
Pedro Souza <pedrosouzaa.024@gmail.com>
2025-08-27 01:27:03 -0300

    Adicionando correção ao exercício

--- revisoes/Par-impar/script.js
+++ revisoes/Par-impar/script.js
@@ -4,15 +4,13 @@ function verificaParImpar() {
     var numero = Number(valdig.value)
     
     //window.alert(numero)
-
-
-    ///poderia ter sido % 2 que ai eu poderia fazer
-    ///da seguinte forma --- if (numero % 2 == 0)
-
-    if (numero % 10 == 0 || numero % 10 == 2 || numero % 10 == 4 || numero % 10 == 6 || numero % 10 == 8){
+    ///poderia ter sido % 2 que ai eu poderia fazer da seguinte forma --- if (numero % 2 == 0)
+    //if (numero % 10 == 0 || numero % 10 == 2 || numero % 10 == 4 || numero % 10 == 6 || numero % 10 == 8){
+     
+    if ( numero % 2 == 0){
         resultado.innerHTML = `O número ${numero} é par`
     } else {
         resultado.innerHTML = `O número ${numero} é ímpar`
     }
-    
+
 }
