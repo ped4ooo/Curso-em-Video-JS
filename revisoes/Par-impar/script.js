ff3dce0fd2c3e8243cbd13ac674c910f3d1063ee
Pedro Souza <pedrosouzaa.024@gmail.com>
2025-08-27 01:22:26 -0300

    Adiciona comentário como sugestão de melhoria

--- revisoes/Par-impar/script.js
+++ revisoes/Par-impar/script.js
@@ -5,6 +5,10 @@ function verificaParImpar() {
     
     //window.alert(numero)
 
+
+    ///poderia ter sido % 2 que ai eu poderia fazer
+    ///da seguinte forma --- if (numero % 2 == 0)
+
     if (numero % 10 == 0 || numero % 10 == 2 || numero % 10 == 4 || numero % 10 == 6 || numero % 10 == 8){
         resultado.innerHTML = `O número ${numero} é par`
     } else {
