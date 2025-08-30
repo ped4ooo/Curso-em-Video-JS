62565ffc49d2ad522b93f0002360226a02719049
Pedro Souza <pedrosouzaa.024@gmail.com>
2025-08-30 01:40:26 -0300

    Adiciona exercício da aula sobre repetição com correções

--- MODULO E/aula13.js
+++ MODULO E/aula13.js
@@ -1,12 +1,13 @@
-/* Estrutura de repetição com teste no início
+//Estrutura de repetição com teste no início
 var contagem = 0
 while (contagem < 9){
     console.log('Olá, tudo certo?')
     contagem++
 }
 console.log(`Encerrou! A contagem parou em ${contagem}.`)
-*/
 
+
+//Estrutura de repetição com teste no final
 var contando = 0
 do {
     console.log(`A contagem está em ${contando}`)
