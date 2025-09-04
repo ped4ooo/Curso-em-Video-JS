782006943944670dd5258fe9cc54c7e919481441
Pedro Souza <pedrosouzaa.024@gmail.com>
2025-09-03 23:13:40 -0300

    Adiciona exercício sobre Array/Vetor

---
+++ MODULO F/anotacoes-aula15.js
@@ -0,0 +1,29 @@
+/// Aprendendo a usar Vetore/Array
+
+// Aprendendo a colocar valores em um array
+let num = [4, 9, 5]
+console.log(`O valor do vetor é = ${numeros}`)
+
+
+
+// Como contar quantos elementos/posições um array tem
+let numeros = [6, 2, 4, 9, 1, 8]
+console.log(numeros.length)
+
+
+
+// Como ordenar em ordem crescente valores dentro de um array
+let testeOrdem = [6, 2, 4, 9, 1, 8]
+console.log(testeOrdem.sort())
+
+
+
+// Como adicionar um valor na última posição de um arrau
+let adicionandoVal = [6, 2, 4, 9, 1,8]
+adicionandoVal.push(3)
+console.log(adicionandoVal)
+
+// Colocando tipos de dados diferentes em um array
+let primeiroVetor = ["Pedro", 21, "Admin"]
+console.log(primeiroVetor[2])
+
